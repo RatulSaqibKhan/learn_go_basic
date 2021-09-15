50020df6e7831c3bb802fb563dfb66e360af458f
RatulSaqibKhan <ratulkhan.jhenidah@gmail.com>
2021-09-16 02:25:33 +0600

    Calculate Mean Value

--- calculateMinValue.go
+++ calculateMinValue.go
@@ -4,18 +4,17 @@ package main
 import "fmt"
 
 func main() {
-	var x int
-	var y int
+	var x float64
+	var y float64
 
-	x = 1
-	y = 2
+	x = 1.0
+	y = 2.0
 
 	fmt.Printf("x=%v, type of %T\n", x, x)
 	fmt.Printf("x=%v, type of %T\n", y, y)
 
-	var mean int
+	var mean float64
 
 	mean = ( x + y ) / 2
 	fmt.Printf("x=%v, type of %T\n", mean, mean)
 }
-
