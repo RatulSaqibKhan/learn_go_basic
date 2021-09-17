b63c5fd8aeeda091b43f9ad51217cea6ccf76f7e
RatulSaqibKhan <ratulkhan.jhenidah@gmail.com>
2021-09-17 23:11:06 +0600

    Sprint

---
+++ sprint/sprintf.go
@@ -0,0 +1,12 @@
+// fmt.Sprint example
+package main
+
+import ("fmt")
+
+func main() {
+	n := 42
+	s := fmt.Sprintf("%d", n)
+
+	fmt.Printf("s = %q (type %T)\n", s, s)
+	fmt.Printf("s = %s (type %T)\n", s, s)
+}
