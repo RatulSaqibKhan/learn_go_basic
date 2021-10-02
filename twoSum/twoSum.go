29f4f3084b9fcda554612806b9d76bf3e872683d
RatulSaqibKhan <ratulkhan.jhenidah@gmail.com>
2021-10-02 10:58:31 +0600

    two sum another way

--- twoSum/twoSum.go
+++ twoSum/twoSum.go
@@ -12,21 +12,45 @@ package main
 
 import "fmt"
 
+// O(n2)
 func twoSum(nums []int, target int) []int {
-	var result []int 
+	var result []int
+
 	for i, num1 := range nums {
-			for j, num2 := range nums[i+1:] {
-					if num1 + num2 == target {
-						result = []int {i, i+j+1}
-						break
-					}
+		for j, num2 := range nums[i+1:] {
+			if num1+num2 == target {
+				result = []int{i, i + j + 1}
+				break
 			}
+		}
 	}
 	return result
 }
 
+// O(n)
+func twoSumUsingMap(nums []int, target int) []int {
+	resultMap := map[int]int{
+		nums[0]: 0,
+	}
+	var result []int
+
+	for i, num1 := range nums {
+		required_num := target - num1
+
+		searchVal, ok := resultMap[required_num]
+		if ok && i != searchVal {
+			result = []int{searchVal, i}
+			break
+		} else {
+			resultMap[num1] = i
+		}
+	}
+
+	return result
+}
+
 func main() {
-	res := twoSum([]int {3,2,1}, 5)
+	res := twoSumUsingMap([]int{1, 6, 2, 3}, 5)
 
 	fmt.Println(res)
 }
